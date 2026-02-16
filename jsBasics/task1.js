41ece185af14c82a5f6861b2abc960c3f1f15852
Valentyna <moonight82@gmail.com>
2026-02-16 09:53:49 +0200

    Adding homework

---
+++ jsBasics/task1.js
@@ -0,0 +1,14 @@
+let strType = "Here is a string type";
+console.log(strType);
+
+let numType = 78;
+console.log(numType);
+
+let boolType = true;
+console.log(boolType);  
+
+let nullType = null;
+console.log(nullType);
+
+let undefinedType;
+console.log(undefinedType);
---
+++ jsBasics/task4_3.js
@@ -0,0 +1,6 @@
+let radius = 15;
+let height = 25;
+
+const volume = Math.PI * radius ** 2 * height;
+
+console.log(volume.toFixed(2));
