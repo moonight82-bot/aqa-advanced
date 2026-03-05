6d2d53533081a9b7ff9c0e7bc722d26d8864ecc2
Valentyna <moonight82@gmail.com>
2026-03-05 21:40:25 +0200

    Add Homatask 8

---
+++ Hometask_8/hometask_8_3.js
@@ -0,0 +1,3 @@
+const initialArray = [10, 20, 30, 40, 50];
+const sum = initialArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+console.log("Сума елементів масиву:", sum);
---
+++ Hometask_8/hometask_8_4.js
@@ -0,0 +1,3 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newArray=myArray.filter(num => num % 2 === 0);
+console.log("Парні числа:", newArray);
